664b24ee5dda75d4ebcbfd107068b6e78053a24a
Jenny Ardrey <jenny@ardrey.co.uk>
2019-10-04 19:12:53 +0100

    made design changes and added js for form

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+document.getElementById("submit_now").onclick = function() {
+    var name = document.getElementById("name");
+    var email = document.getElementById("mail");
+    var msg = document.getElementById("msg");
+    window.alert(name.value + " , " + email.value + " , " + msg.value);
+}
 
-//function hover() {
-//    document.getElementById("logoimg").src = "GlassesLogoCool.jpg";
-//}
+
