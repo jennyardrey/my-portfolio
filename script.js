7effaf8966867e38a077424fc806489a0e692763
Jenny Ardrey <jenny@ardrey.co.uk>
2019-10-07 20:59:11 +0100

    further style tweaks and js

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-document.getElementById("submit_now").onclick = function() {
+/* document.getElementById("submit_now").onclick = function() {
+    var name = document.getElementById("name");
+    var email = document.getElementById("mail");
+    var msg = document.getElementById("msg");
+    window.alert(name.value + " , " + email.value + " , " + msg.value);
+} */
+
+function formSub() {
     var name = document.getElementById("name");
     var email = document.getElementById("mail");
     var msg = document.getElementById("msg");
     window.alert(name.value + " , " + email.value + " , " + msg.value);
 }
+
+
+var submit = document.getElementById("submit_now");
+
+submit.addEventListener("click", formSub, false);
+
+var clear = document.getElementById("clear_now");
+
+function formClear() {
+    window.alert("Fill in the form again dumbass");
+}
+
+clear.addEventListener("click", formClear, false);
 
 
