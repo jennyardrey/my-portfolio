a90c4a534e9161d798f742922561b49283a223c4
Jenny Ardrey <jenny@ardrey.co.uk>
2019-10-26 20:48:48 +0100

    made nav more responsive and hid nav bar behind burger menu icon when on smaller screens

--- script.js
+++ script.js
@@ -6,18 +6,18 @@
 } */
 
 function formSub() {
-    var name = document.getElementById("name");
-    var email = document.getElementById("mail");
-    var msg = document.getElementById("msg");
+    const name = document.getElementById("name");
+    const email = document.getElementById("mail");
+    const msg = document.getElementById("msg");
     window.alert(name.value + " , " + email.value + " , " + msg.value);
 }
 
 
-var submit = document.getElementById("submit_now");
+const submit = document.getElementById("submit_now");
 
 submit.addEventListener("click", formSub, false);
 
-var clear = document.getElementById("clear_now");
+const clear = document.getElementById("clear_now");
 
 function formClear() {
     window.alert("Fill in the form again dumbass");
